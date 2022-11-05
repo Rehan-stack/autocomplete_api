e60722ed17314198fc4affd30ed6da29a839f396
Rehan-stack <rehanahmedahmed007@gmail.com>
2022-11-05 12:00:45 +0500

    Master

--- autocomplete_api/settings.py
+++ autocomplete_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=2o9bg!ym-f-sjkyt1hy1s61zg6@j-5@)$m^sy1j93@yo4uu%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
